b91fc83f04c84308ad2f19244f805dec12168e58
Hana Spoustová <bernreih@gmail.com>
2024-02-09 21:39:18 +0100

    uloha 5

--- src/components/Uloha5/index.jsx
+++ src/components/Uloha5/index.jsx
@@ -1,4 +1,5 @@
 import './hamburger.css';
+import { useState } from 'react';
 
 // Zadání 1: Přichystejte si stavovou proměnnou s výchozí hodnotou `false`, ve které bude uloženo, jestli je menu otevřené nebo nikoliv.
 // Zadání 2: Po kliknutí na tlačítko změňte stav z `true` na `false` nebo obráceně.
@@ -6,27 +7,39 @@ import './hamburger.css';
 // Zadání 4: Pokud je menu zavřené nechte mu jen třídu `hamburger`. Pro otevřené přidejte navíc `hamburger--otevrene`.
 
 export const Uloha5 = () => {
+  const [open, setOpen] = useState(false);
+
+  const handleClick = function () {
+    if (!open) setOpen(true);
+    else setOpen(false);
+  };
+
+  const menuOpen = open ? 'hamburger hamburger--otevrene' : 'hamburger';
+
   return (
     <>
-      <button className="hamburger" aria-label="menu">
+      <button className={menuOpen} aria-label="menu" onClick={handleClick}>
         <span></span>
         <span></span>
         <span></span>
       </button>
-      <ul>
-        <li>
-          <a href="#o-nas">O nás</a>
-        </li>
-        <li>
-          <a href="#co-delame">Co děláme</a>
-        </li>
-        <li>
-          <a href="#kontakty">Kontakty</a>
-        </li>
-        <li>
-          <a href="#cenik">Ceník</a>
-        </li>
-      </ul>
+
+      {open ? (
+        <ul>
+          <li>
+            <a href="#o-nas">O nás</a>
+          </li>
+          <li>
+            <a href="#co-delame">Co děláme</a>
+          </li>
+          <li>
+            <a href="#kontakty">Kontakty</a>
+          </li>
+          <li>
+            <a href="#cenik">Ceník</a>
+          </li>
+        </ul>
+      ) : null}
     </>
   );
 };
